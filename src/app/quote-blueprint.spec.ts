22bdcf22691295fba2eb5558b433a57e312f309c
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-07-20 19:34:53 +0300

    added a spec file for blueprint

---
+++ src/app/quote-blueprint.spec.ts
@@ -0,0 +1,7 @@
+import { QuoteBlueprint } from './quote-blueprint';
+export { QuoteBlueprint}
+describe('QuoteBlueprint', () => {
+  it('should create an instance', () => {
+    expect(new QuoteBlueprint()).toBeTruthy();
+  });
+});
